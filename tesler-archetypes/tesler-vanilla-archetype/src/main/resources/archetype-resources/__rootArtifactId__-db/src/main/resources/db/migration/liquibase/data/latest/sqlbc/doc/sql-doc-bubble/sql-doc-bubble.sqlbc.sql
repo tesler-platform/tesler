527b43f2abe2b9231f5677468c5fe24a89494877
abratashev <abratashev@phoenixit.ru>
2020-07-27 02:00:39 +0300

    Remove vanilla and workflow modules from project

--- tesler-archetypes/tesler-vanilla-archetype/src/main/resources/archetype-resources/__rootArtifactId__-db/src/main/resources/db/migration/liquibase/data/latest/sqlbc/doc/sql-doc-bubble/sql-doc-bubble.sqlbc.sql
+++
@@ -1,5 +0,0 @@
-select 1 as id,'НПФ 1' as xcategory, 10 as y, 5 as z, 'Показатель 1 НПФ 1' as val,15 as y1, 500 as z1, 'Показатель 2 НПФ 1' as val1 from dual
-union all
-select  2 as id,'НПФ 2' as xcategory, 5 as y, 15 as z, 'Показатель 1 НПФ 2' as val,20 as y1, 100 as z1, 'Показатель 2 НПФ 2' as val1 from dual
-union all
-select  3 as id,'НПФ 3' as xcategory, 2 as y, 10 as z, 'Показатель 1 НПФ 3' as val,10 as y1, 300 as z1, 'Показатель 2 НПФ 3' as val1 from dual
