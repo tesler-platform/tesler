3f9159541e9ca2024a56adfc6c56b609d7fa746c
Renat Yalalov <renat.yalalov@weigandt-consulting.com>
2021-04-09 12:58:24 +0300

    changed distinct to subquery in filtering

--- tesler-core/src/main/java/io/tesler/core/dao/impl/BaseDAOImpl.java
+++ tesler-core/src/main/java/io/tesler/core/dao/impl/BaseDAOImpl.java
@@ -29,10 +29,15 @@ import io.tesler.core.dao.BaseDAO;
 import io.tesler.core.dao.IPdqExtractor;
 import io.tesler.core.util.filter.provider.ClassifyDataProvider;
 import io.tesler.model.core.dao.impl.JpaDaoImpl;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 import javax.persistence.EntityGraph;
 import javax.persistence.EntityManager;
 import javax.persistence.criteria.CriteriaBuilder;
@@ -40,15 +45,10 @@ import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 import javax.persistence.metamodel.SingularAttribute;
-import lombok.extern.slf4j.Slf4j;
-import org.hibernate.Session;
-import org.hibernate.criterion.CriteriaSpecification;
-import org.hibernate.query.Query;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.data.jpa.domain.Specification;
-import org.springframework.orm.jpa.vendor.Database;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 @Slf4j
 @Repository
@@ -96,19 +96,17 @@ public class BaseDAOImpl extends JpaDaoImpl implements BaseDAO {
 		return getList(cq, root, dtoClazz, defaultSearchSpec, parameters, null);
 	}
 
-	public Predicate getPredicateFromSearchParams(
-			CriteriaBuilder cb,
-			Root<?> root,
+	public <T> Predicate getPredicateFromSearchParams(Root<T> root, CriteriaQuery<?> cq, CriteriaBuilder cb,
 			Class dtoClazz,
 			FilterParameters searchParams
 	) {
-		return MetadataUtils.getPredicateFromSearchParams(cb, root, dtoClazz, searchParams, providers);
+		return MetadataUtils.getPredicateFromSearchParams(root, cq, cb, dtoClazz, searchParams, providers);
 	}
 
 	@Override
-	public Long getCount(
+	public <T> Long getCount(
 			CriteriaQuery<Long> cq,
-			Root<?> root,
+			Root<T> root,
 			Class dtoClazz,
 			Predicate defaultSearchSpec,
 			QueryParameters queryParameters
@@ -125,7 +123,7 @@ public class BaseDAOImpl extends JpaDaoImpl implements BaseDAO {
 			pdqSearchSpec = (root1, cq1, cb1) -> cb1.and();
 		}
 		cq.select(cb.count(root));
-		Predicate searchParamsPredicate = getPredicateFromSearchParams(cb, root, dtoClazz, searchParams);
+		Predicate searchParamsPredicate = getPredicateFromSearchParams(root, cq, cb, dtoClazz, searchParams);
 		cq.where(cb.and(defaultSearchSpec, searchParamsPredicate, pdqSearchSpec.toPredicate(root, cq, cb)));
 		return entityManager.createQuery(cq).getSingleResult();
 	}
@@ -152,7 +150,7 @@ public class BaseDAOImpl extends JpaDaoImpl implements BaseDAO {
 		if (pdqSearchSpec != null) {
 			predicates.add(pdqSearchSpec.toPredicate(root, cq, cb));
 		}
-		predicates.add(getPredicateFromSearchParams(cb, root, dtoClazz, parameters));
+		predicates.add(getPredicateFromSearchParams(root, cq, cb, dtoClazz, parameters));
 		cq.where(cb.and(predicates.toArray(new Predicate[0])));
 		return entityManager.createQuery(cq).getSingleResult();
 	}
@@ -177,20 +175,8 @@ public class BaseDAOImpl extends JpaDaoImpl implements BaseDAO {
 		if (pdqSearchSpec == null) {
 			pdqSearchSpec = (root1, cq1, cb1) -> cb1.and();
 		}
-		int joinsInRoot = root.getJoins().size();
-		Predicate searchParamsPredicate = getPredicateFromSearchParams(cb, root, dtoClazz, filter);
 
-		// TODO: Narrow it down based on criteria
-		boolean distinctRequired = root.getJoins().size() > joinsInRoot;
-		/**
-		 * Non-Oracle DB can handle distinct for CLOBs so it can be applied
-		 * as sql clause.
-		 *
-		 * @see https://hibernate.atlassian.net/browse/HHH-3606
-		 */
-		if (!this.primaryDatabase.equals(Database.ORACLE) && distinctRequired) {
-			cq.distinct(true);
-		}
+		Predicate searchParamsPredicate = getPredicateFromSearchParams(root, cq, cb, dtoClazz, filter);
 
 		if (cq.getRestriction() != null) {
 			cq.where(cb.and(
@@ -207,32 +193,10 @@ public class BaseDAOImpl extends JpaDaoImpl implements BaseDAO {
 			));
 		}
 		MetadataUtils.addSorting(dtoClazz, root, cq, cb, sort);
-
-		// query.setHint("javax.persistence.fetchgraph", fetchGraph)
-		// more correct but causes performance troubles
 		applyGraph(root, fetchGraph);
 
 		Query<T> query = entityManager.unwrap(Session.class).getSession().createQuery(cq);
 		applyPaging(query, parameters.getPage());
-
-		/**
-		 * Joins from filters (e.g. multivalue fields) can cause duplications in result set,
-		 * which are handled by applying `distinct` in memory.
-		 *
-		 * Deprecated `setResultTransformer` usage due to DB-level `distinct` clause is not applicable
-		 * because it does not support CLOBs and criteria query's `distinct()` can't properly handle
-		 * `QueryHints.HINT_PASS_DISTINCT_THROUGH` in nested (i.e. paginated) requests.
-		 *
-		 * Downside is that when distinct actually does filters out duplicates the number of records
-		 * in result set will be less than required by pagination parameters.
-		 *
-		 * @see https://hibernate.atlassian.net/browse/HHH-11726
-		 * @see https://discourse.hibernate.org/t/hibernate-resulttransformer-is-deprecated-what-to-use-instead/232
-		 *
-		 */
-		if (this.primaryDatabase.equals(Database.ORACLE) && distinctRequired) {
-			query.setResultTransformer(CriteriaSpecification.DISTINCT_ROOT_ENTITY);
-		}
 
 		return ResultPage.of(query.getResultList(), parameters.getPage());
 	}
