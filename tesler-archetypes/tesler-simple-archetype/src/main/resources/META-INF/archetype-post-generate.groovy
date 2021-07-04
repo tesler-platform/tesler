6f02a8580c899fdb926f8cb52f74d9925c2427aa
Fedor Gubarev <gubarevfyodor@gmail.com>
2021-07-05 01:00:07 +0300

    upgrade to @tesler-ui/cra-template-typescript@0.3.0-alpha3

--- tesler-archetypes/tesler-simple-archetype/src/main/resources/META-INF/archetype-post-generate.groovy
+++ tesler-archetypes/tesler-simple-archetype/src/main/resources/META-INF/archetype-post-generate.groovy
@@ -6,7 +6,7 @@ import java.nio.file.Paths
 def launcher = System.properties['os.name'].toLowerCase().contains('windows') ? "npx.cmd" : "npx"
 
 // Run create-react-app with latest @tesler-ui/cra-template-typescript
-def cmd = "${launcher} create-react-app ${request.outputDirectory}/${request.artifactId}/ui --template @tesler-ui/cra-template-typescript@0.3.0-alpha1"
+def cmd = ["${launcher}", "create-react-app", "${request.outputDirectory}/${request.artifactId}/ui", "--template", "@tesler-ui/cra-template-typescript@0.3.0-alpha3"]
 def proc = cmd.execute(null, new File(request.outputDirectory))
 proc.consumeProcessOutput(System.out, System.err)
 proc.waitFor()
